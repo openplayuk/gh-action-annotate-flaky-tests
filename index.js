14a715c204571058becc19e7f192b562efddece7
Will <willb@openplay.co.uk>
2023-10-02 17:14:51 +0100

    updated action to use summary instead of annotate

--- index.js
+++ index.js
@@ -18,34 +18,11 @@ async function run() {
     const annotations = [];
 
     if (jsonData) {
+      core.summary.addHeading('Flaky Tests');
       for (const item of jsonData) {
-        annotations.push({
-          title: item.title,
-          message: item.message ,
-          path: item.file,
-          start_line: item.line,
-          end_line: item.line,
-          annotation_level: item.annotation_level,
-        });
+        core.summary.addDetails(':yellow_circle:	' + item.title, item.file);
       }
-
-      if (annotations.length > 0) {
-        const octokit = github.getOctokit(core.getInput('github_token'));
-
-        try {     
-          annotations.forEach(element => {
-            core.warning(element.title + " is marked as flaky, consider rerunning");
-          });
-        } catch (error) {
-          console.error('API Error:', error.message);
-          core.setFailed('Failed to create annotations.');
-        }
-      } else {
-        console.log('No annotations to create.');
-      }
-    } else {
-      console.log(jsonData);
-      core.setFailed('No json content available');
+      core.summary.write();
     }
   } catch (error) {
     core.setFailed(error.message);
